363353b2083009baad9b1f2979a5362721304bfd
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-10-18 00:20:51 +0200

    first commit: Load initial files

---
+++ Custom Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Custom Timer
+//
+//  Created by Marcin Jędrzejak on 18/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Custom Timer/Custom_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_TimerApp.swift
+//  Custom Timer
+//
+//  Created by Marcin Jędrzejak on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Custom_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
